ddc02f2904cfc7c5f928a6595357c9310d59f9b3
merdan <merdanbebitow07@gmail.com>
2026-03-07 12:07:48 +0300

    My repository

---
+++ DoublyLinkedList/DoublyNode.cs
@@ -0,0 +1,13 @@
+namespace DoublyLinkedList;
+
+public class DoublyNode<T>
+{
+    public T Data { get; set; }
+    public DoublyNode<T> Next { get; set; }
+    public DoublyNode<T> Previous { get; set; }
+
+    public DoublyNode(T data)
+    {
+        Data = data;
+    }
+}
